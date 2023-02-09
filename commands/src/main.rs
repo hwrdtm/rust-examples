cac0b108634c4b92e9536278b443759236723a26
Howard Tam <howard.tam.95@gmail.com>
2023-02-08 20:50:34 -0800

    Implement commands example

--- commands/src/main.rs
+++ commands/src/main.rs
@@ -1,3 +1,22 @@
+use std::os::fd::{IntoRawFd, FromRawFd};
+use std::process::{Command, Stdio};
+use std::io::{Write, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = std::fs::File::create("./node.txt").unwrap();
+    
+    // Use Command and Stdio piped to run: echo -n 'thing' | base64
+    let echo_cmd = Command::new("echo")
+        .arg("-n")
+        .arg("thing")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let encode_cmd = Command::new("base64")
+        .stdin(Stdio::from(echo_cmd.stdout.unwrap()))
+        // Write the result of encode_cmd to file.
+        .stdout(Stdio::from(file))
+        .spawn()
+        .unwrap();
 }
