0063825fe085eac7a9efce58d904b03e2b8a3dde
Howard Tam <howard.tam.95@gmail.com>
2023-09-19 17:28:05 -0700

    Make example more interesting

--- delayed-spray-and-pray/src/main.rs
+++ delayed-spray-and-pray/src/main.rs
@@ -3,7 +3,7 @@
 use delayed_spray_and_pray::delayed_spray_and_pray;
 use log::info;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 struct SomeStruct {
     pub ret_val: u64,
 }
@@ -12,17 +12,23 @@ struct SomeStruct {
 async fn main() -> Result<(), anyhow::Error> {
     env_logger::init();
 
+    let client = reqwest::Client::builder()
+        .build()
+        .unwrap();
+
     // Test 1: other future finishes first.
     let res = delayed_spray_and_pray(
         500,
         "main_task",
-        long_running_task(SomeStruct { ret_val: 10 }),
+        client.get("https://www.google.com/").send(),
         "backup_task",
-        long_running_task(SomeStruct { ret_val: 4 }),
+        async {
+            client.get("https://google.com/").send()
+        }.await,
     )
     .await;
     assert!(res.is_ok());
-    assert_eq!(res.unwrap(), 4);
+    println!("{:?}", res.unwrap());
 
     // Test 2: main future finishes first.
     let res = delayed_spray_and_pray(
@@ -34,18 +40,18 @@ async fn main() -> Result<(), anyhow::Error> {
     )
     .await;
     assert!(res.is_ok());
-    assert_eq!(res.unwrap(), 4);
+    assert_eq!(res.unwrap(), SomeStruct { ret_val: 4 });
 
     let _ = long_running_task(SomeStruct { ret_val: 10 }).await;
 
     Ok(())
 }
 
-async fn long_running_task(some_struct: SomeStruct) -> Result<u64, anyhow::Error> {
+async fn long_running_task(some_struct: SomeStruct) -> Result<SomeStruct, anyhow::Error> {
     info!("Starting long running task");
 
     // Sleep for 10 seconds
     tokio::time::sleep(std::time::Duration::from_secs(some_struct.ret_val)).await;
 
-    Ok(some_struct.ret_val)
+    Ok(some_struct.clone())
 }
