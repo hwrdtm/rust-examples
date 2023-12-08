e4f32b7ee5f4dc42f9e17959920b9985012dd690
Howard Tam <howard.tam.95@gmail.com>
2023-12-09 04:07:43 +0800

    Add trait object example to fn-mut-arg example

--- fn-mut-arg/src/main.rs
+++ fn-mut-arg/src/main.rs
@@ -1,5 +1,7 @@
 use std::{fmt::Debug, future::Future};
 
+use futures::future::BoxFuture;
+
 #[tokio::main]
 async fn main() {
     // Demonstrate the simple mapping functionality.
@@ -12,7 +14,7 @@ async fn main() {
     store.execute();
 
     // Demonstrate the async mapping functionality.
-    let mut async_store = AsyncStore {
+    let mut async_store = AsyncStoreGeneric {
         items: vec!["c".to_string(), "d".to_string(), "e".to_string()],
         mapper_fn: |tup: (usize, String)| async move {
             println!("({}): {}", tup.0, tup.1);
@@ -23,6 +25,19 @@ async fn main() {
         },
     };
     async_store.execute().await;
+
+    // Demonstrate the async mapping functionality.
+    let mut async_store_trait_object = AsyncStoreTraitObject {
+        items: vec!["f".to_string(), "g".to_string(), "h".to_string()],
+        mapper_fn: Box::new(|tup: (usize, String)| Box::pin(async move {
+            println!("({}): {}", tup.0, tup.1);
+            if tup.0 >= 1 {
+                some_sleep_func(&tup.1).await;
+            }
+            Ok::<i32, anyhow::Error>(1)
+        }) as BoxFuture<'static, Result<i32, anyhow::Error>>)
+    };
+    async_store_trait_object.execute().await;
 }
 
 pub struct SimpleStore {
@@ -38,7 +53,7 @@ impl SimpleStore {
     }
 }
 
-pub struct AsyncStore<M>
+pub struct AsyncStoreGeneric<M>
 where
     M: Mapper,
 {
@@ -46,11 +61,26 @@ where
     pub mapper_fn: M,
 }
 
-impl<M> AsyncStore<M>
+impl<M> AsyncStoreGeneric<M>
 where
     M: Mapper,
     <M as Mapper>::Error: Debug,
 {
+    pub async fn execute(&mut self) {
+        for (i, item) in self.items.iter().enumerate() {
+            if let Err(e) = self.mapper_fn.run((i, item.to_owned())).await {
+                println!("Error: {:?}", e);
+            }
+        }
+    }
+}
+
+pub struct AsyncStoreTraitObject {
+    pub items: Vec<String>,
+    pub mapper_fn: Box<dyn Mapper<Item = i32, Error = anyhow::Error, Future = BoxFuture<'static, Result<i32, anyhow::Error>>>>,
+}
+
+impl AsyncStoreTraitObject {
     pub async fn execute(&mut self) {
         for (i, item) in self.items.iter().enumerate() {
             if let Err(e) = self.mapper_fn.run((i, item.to_owned())).await {
@@ -79,6 +109,6 @@ impl<R, E, T: Future<Output = Result<R, E>>, F: FnMut((usize, String)) -> T> Map
 }
 
 async fn some_sleep_func(s: &str) {
-    std::thread::sleep(std::time::Duration::from_secs(1));
     println!("some_sleep_func: {}", s);
+    std::thread::sleep(std::time::Duration::from_secs(1));
 }
