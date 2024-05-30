22190681dd19ff07e86de67e50cb46184eb712e2
Howard Tam <howard.tam.95@gmail.com>
2024-05-30 11:33:13 -0700

    Write each request as JSON to local file

--- simple-observability-pipeline/src/proxy_server.rs
+++ simple-observability-pipeline/src/proxy_server.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use opentelemetry_proto::tonic::collector::metrics::v1::metrics_service_server::{
     MetricsService, MetricsServiceServer,
 };
@@ -9,6 +11,8 @@ use tokio::net::UnixListener;
 use tokio_stream::wrappers::UnixListenerStream;
 use tonic::{transport::Server, Request, Response, Status};
 
+const LOCAL_FILE_OUT: &str = "./otel_proxy_server.log";
+
 #[derive(Debug, Default)]
 pub struct OTELProxyServer {}
 
@@ -19,6 +23,14 @@ impl MetricsService for OTELProxyServer {
         request: Request<ExportMetricsServiceRequest>,
     ) -> Result<Response<ExportMetricsServiceResponse>, Status> {
         println!("Got a request: {:?}", request);
+
+        // Write the request as JSON to a log file
+        let mut file = std::fs::OpenOptions::new()
+            .append(true)
+            .open(LOCAL_FILE_OUT)
+            .unwrap();
+        let request_str = serde_json::to_string(request.get_ref()).unwrap();
+        file.write_all(request_str.as_bytes()).unwrap();
 
         let reply = ExportMetricsServiceResponse {
             partial_success: None,
@@ -36,6 +48,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if std::fs::metadata(DEFAULT_SOCK).is_ok() {
         std::fs::remove_file(DEFAULT_SOCK)?;
     }
+
+    // If the log file already exists, remove it
+    if std::fs::metadata(LOCAL_FILE_OUT).is_ok() {
+        std::fs::remove_file(LOCAL_FILE_OUT)?;
+    }
+    // Create the log file
+    std::fs::File::create(LOCAL_FILE_OUT)?;
 
     let uds = UnixListener::bind(DEFAULT_SOCK)?;
     let uds_stream = UnixListenerStream::new(uds);
